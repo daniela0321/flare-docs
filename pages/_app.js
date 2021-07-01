d94586ad913254196e3fb539e865455fc2aba91f
Daniela <daniela.vinaccia@gmail.com>
2021-07-01 14:05:43 +0300

    Move Layout to _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,19 @@
-import Layout from "../components/layout/Layout"
+import Header from "../components/layout/Header"
+import Footer from "../components/layout/Footer"
 // import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/bootstrap.theme.scss'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Layout>
-    <Component {...pageProps} />
-  </Layout>
+  return (
+    <div>
+      <Header />
+      <main>
+        <Component {...pageProps} />
+      </main>
+      <Footer />
+    </div>
+  )
 }
 
 export default MyApp
