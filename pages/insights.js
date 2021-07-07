b561481793a111e1c34c6d18f81ee0caf11577f4
Daniela <daniela.vinaccia@gmail.com>
2021-07-07 22:48:01 +0300

    Build Insights page further

--- pages/insights.js
+++ pages/insights.js
@@ -1,11 +1,82 @@
+import Head from "next/head"
+import React from "react"
+import { Fragment } from "react"
+import { Container } from "react-bootstrap"
+import { readdirSync, readFileSync } from 'fs'
+import path from 'path'
+import matter from "gray-matter"
+import ReactMarkdown from 'react-markdown'
 import PageTitle from "../components/layout/PageTitle"
+import InsightList from "../components/insights/InsightList"
 
-function Insights() {
+export default function Insights(props) {
     return (
-        <PageTitle
-            title="Insights"
-        />
+        <Fragment>
+            <Head>
+                <title>Flare Hub Insights</title>
+                <meta
+                    name="description"
+                    content="Browse the list of Insights which address common frameworks and theories used when building digital solutions"
+                />
+            </Head>
+            <PageTitle
+                title="Insights"
+            />
+            <Container fluid>
+                <InsightList
+                    insights={props.insights}
+                />
+            </Container>
+        </Fragment>
     )
 }
 
-export default Insights
+// Return  filenames for all Insights
+function getFileNames() {
+    // path.join() method joins all given path segments together
+    // process.cwd() gives the directory where Next.js is being executed
+    const insightFolder = path.join(process.cwd(), "Content")
+    //  Return an array of filenames (readdirSync lists the filesnames of the directory)
+    return readdirSync(insightFolder)
+}
+
+// For all Insights, return the necessary content for the card
+export async function getStaticProps(context) {
+
+    // Get file names
+    const fileNames = getFileNames()
+
+    // console.log("fileNames:", fileNames)
+
+    const insights = fileNames.map((fileName) => {
+
+        // Construct file path
+        const filePath = path.join(process.cwd(), `Content/${fileName}`)
+
+        //  Import .md file using the file path (readFileSync returns the content of a file)
+        const content = readFileSync(filePath, "utf8")
+
+        // Parse .md data through "matter"
+        // This holds the data between `---` from the .md file
+        const frontmatter = matter(content)
+
+        const slug = fileName.replace(".md", "")
+
+        return {
+            image: frontmatter.data.image,
+            title: frontmatter.data.title,
+            body: frontmatter.content.slice(0, 200) + "...",
+            buttonLink: "/insights/" + slug,
+            id: slug,
+        }
+    })
+
+    // Pass data to component props
+    return {
+        props: {
+            insights: insights
+        },
+        revalidate: 10
+    }
+}
+
