7444f5fef7707facf8357c5ba094a3cd32fe64e8
Daniela <daniela.vinaccia@gmail.com>
2021-07-20 18:44:23 +0300

    Modify links

--- components/insights/TocH3.js
+++ components/insights/TocH3.js
@@ -1,11 +1,10 @@
-import classes from "./TocH3.module.css"
 import slugify from "../../utils/slugify"
 
 function TocH3({ children }) {
 
     const path = "#" + slugify(children[0])
 
-    return <li className="mb-2 text-muted small-font reduce-lineheight"><a href={path} className="custom-links">{children}</a></li>
+    return <li className="mb-2 text-muted small-font reduce-lineheight"><a href={path} className="sidelink">{children}</a></li>
 }
 
 export default TocH3
