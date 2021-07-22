2aac5de5a12432b01bfce6b19d32c7d0edc58074
Daniela <daniela.vinaccia@gmail.com>
2021-07-22 16:17:45 +0300

    Markdown image modal: add close icon

---
+++ components/insights/Close.js
@@ -0,0 +1,14 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+
+function Close() {
+
+    return (
+        <FontAwesomeIcon
+            icon={faTimes}
+            size="2x"
+        />
+    )
+}
+
+export default Close
