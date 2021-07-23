2d99af43b0fd4130a4defcae7b5142f7cf281092
Daniela <daniela.vinaccia@gmail.com>
2021-07-23 18:04:13 +0300

    Contact page: fix form message field

--- pages/contact.js
+++ pages/contact.js
@@ -29,6 +29,7 @@ function Contact() {
 
                 <Row className="my-5 pb-5 d-flex flex-row justify-content-center">
                     {/* Check in which way to do the form validation */}
+                    {/* How about the I'm not a robot functionality (recaptcha) */}
                     <Form className="px-4 pt-4 pb-5 mx-2 border rounded">
                         <Row className="mb-2">
                             {/* One or two columns in mobile? */}
@@ -49,9 +50,9 @@ function Contact() {
                             <Form.Control placeholder="Enter email" />
                         </Form.Group>
 
-                        <Form.Group className="mb-5" controlId="formGridMessage">
+                        <Form.Group className="mb-4" controlId="form.ControlTextarea1">
                             <Form.Label>Feedback or message</Form.Label>
-                            <Form.Control placeholder="Enter feedback or message" />
+                            <Form.Control as="textarea" rows={6} placeholder="Enter feedback or message" />
                         </Form.Group>
 
                         <Form.Group className="mb-5" id="formGridCheckbox">
