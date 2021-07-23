6737eab57453ecc5a88eb3e60e5c96d094007600
Daniela <daniela.vinaccia@gmail.com>
2021-07-23 15:46:12 +0300

    Modify margins

--- components/layout/PageTitle.js
+++ components/layout/PageTitle.js
@@ -3,7 +3,7 @@ import { Container, Row } from "react-bootstrap"
 function PageTitle(props) {
     return (
         <Container className="py-5" >
-            <Row className="pt-3 px-2 d-flex flex-row justify-content-center">
+            <Row className="pt-4 px-2 d-flex flex-row justify-content-center">
                 <h1>{props.title}</h1>
             </Row>
         </Container >
