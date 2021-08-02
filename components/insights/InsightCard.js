ba71f587e1b4a7a12ef0c183539027f792916d82
Daniela <daniela.vinaccia@gmail.com>
2021-08-02 16:17:38 +0300

    Insight cards: add link around them + replace buttons

--- components/insights/InsightCard.js
+++ components/insights/InsightCard.js
@@ -1,18 +1,22 @@
-// import { useRouter } from "next/router"
+import classes from "./InsightCard.module.css"
 import { Card, Button } from "react-bootstrap"
-import ReactMarkdown from 'react-markdown'
+import Link from 'next/link'
 
 function InsightCard({ image, title, excerpt, buttonLink }) {
 
     return (
-        <Card className="mx-2 mb-3" style={{ width: '22rem' }}>
-            <Card.Img variant="top" src={image} alt={title} />
-            <Card.Body>
-                <Card.Title>{title}</Card.Title>
-                <Card.Text>{excerpt}</Card.Text>
-                <Button href={buttonLink} variant="primary">Read more</Button>
-            </Card.Body>
-        </Card>
+        <Link href={buttonLink} passHref>
+            <a className={classes.cardlink}>
+                <Card className="mx-2 mb-3 h-100" style={{ width: '22rem' }}>
+                    <Card.Img variant="top" src={image} alt={title} />
+                    <Card.Body>
+                        <Card.Title>{title}</Card.Title>
+                        <Card.Text>{excerpt}</Card.Text>
+                        <Card.Text className="font-semi-bold text-info">Read more...</Card.Text>
+                    </Card.Body>
+                </Card>
+            </a>
+        </Link>
     )
 }
 
