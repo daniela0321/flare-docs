3f6e9ae239e2f802ccbc6016d6ecf387367b1b54
Daniela <daniela.vinaccia@gmail.com>
2021-08-03 10:14:57 +0300

    TOC: modify margins and line height

--- components/insights/TocH2.js
+++ components/insights/TocH2.js
@@ -4,7 +4,7 @@ function TocH2({ children }) {
 
     const path = "#" + slugify(children[0])
 
-    return <li className="mb-2"><a href={path} className="sidelink">{children}</a></li>
+    return <li className="mb-2 mt-3 reduce-lineheight"><a href={path} className="sidelink">{children}</a></li>
 }
 
 export default TocH2
