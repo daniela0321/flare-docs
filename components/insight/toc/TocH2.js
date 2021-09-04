4613d0f5a7317a2fa89c3fea4eace6c088c946aa
Daniela <daniela.vinaccia@gmail.com>
2021-09-04 17:32:06 +0300

    Insight page: modify table of contents

--- components/insight/toc/TocH2.js
+++ components/insight/toc/TocH2.js
@@ -5,6 +5,10 @@ import slugify from "../../../utils/slugify"
 // Create a clickable list item from the h2 heading with a certain style:
 export default function TocH2({ children }) {
     const path = "#" + slugify(children[0])
-    return <li className="mb-2 mt-3 reduce-lineheight"><a href={path} className="sidelink">{children}</a></li>
+    return <li className="mb-2 mt-3 reduce-lineheight">
+        <a href={path} className="sidelink">
+            {children}
+        </a>
+    </li>
 }
 
--- components/insight/toc/TocH3.js
+++ components/insight/toc/TocH3.js
@@ -5,7 +5,11 @@ import slugify from "../../../utils/slugify"
 // Create a clickable list item from the h3 heading with a certain style:
 export default function TocH3({ children }) {
     const path = "#" + slugify(children[0])
-    return <li className="mb-2 text-muted small-font reduce-lineheight"><a href={path} className="sidelink">{children}</a></li>
+    return <li className="ml-3 mb-2 small-font reduce-lineheight">
+        <a href={path} className="sidelink">
+            {children}
+        </a>
+    </li>
 }
 
 
