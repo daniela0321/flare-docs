2494ef8cdbb31f7351fc322a108944f845568627
igor <igor.honhoff@gmail.com>
2021-09-13 18:47:01 +0300

    fix ENOENT error in Netlify build

--- scripts/generate-cms-preview-css.mjs
+++ scripts/generate-cms-preview-css.mjs
@@ -1,14 +1,16 @@
 import sass from 'sass'
-import { resolve } from 'path'
+import { join } from 'path'
 import { fileURLToPath } from 'url'
 import { writeFileSync } from 'fs'
 
 // Generate preview.css to load in the CMS for previewing insights
 // Use bootstrap.theme.scss to generate the file
+console.log('\n', '-----------------------');
 console.log('Generating preview.css for the cms preview pane...')
 
-const scriptLoc = fileURLToPath(import.meta.url)
-const styles = sass.renderSync({ file: resolve(scriptLoc, '../../styles/bootstrap.theme.scss') })
-writeFileSync(resolve(scriptLoc, '../../public/admin/preview.css'), styles.css)
+// const scriptLoc = fileURLToPath(import.meta.url)
+const styles = sass.renderSync({ file: join(process.cwd(), 'styles/bootstrap.theme.scss') })
+writeFileSync(join(process.cwd(), 'public/admin/preview.css'), styles.css)
 
 console.log('preview.css generated successfully.')
+console.log('-----------------------', '\n');
