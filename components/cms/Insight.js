52a66e9f96d78b107c66c73f315fa3479af6db80
igor <igor.honhoff@gmail.com>
2021-09-14 11:46:00 +0300

    Stop image component from continually updating

--- components/cms/Insight.js
+++ components/cms/Insight.js
@@ -1,4 +1,5 @@
 // External packages:
+import { useRef } from 'react'
 import { Row, Col } from 'react-bootstrap'
 // Internal components
 import Content from '../insight/Content'
@@ -6,6 +7,8 @@ import Content from '../insight/Content'
 
 // Component to show a live preview of the insight whilst editing in the CMS
 export default function Insight({ entry, getAsset }) {
+  // Create persistent component for displaying images using cached sources
+  const PreviewImg = useRef(({ src, alt }) => <img src={getAsset(src)} alt={alt} className="mt-3 w-100" />)
 
   // Transform entry coming from Netlify CMS to the same format as React Markdown
   const frontmatter = entry.toJS().data
@@ -16,7 +19,7 @@ export default function Insight({ entry, getAsset }) {
         <Content
           frontmatter={frontmatter}
           content={frontmatter.body}
-          ImgComponent={({ src, alt }) => <img src={getAsset(src)} alt={alt} className="mt-3 w-100" />}
+          ImgComponent={PreviewImg.current}
           styles={{
             insightMarkdown: 'insightMarkdown',
             insightAuthorEtc: 'insightAuthorEtc',
