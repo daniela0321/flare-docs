18304bdd3d83660f0ccfa05d88fd7efd47ac9729
igor <igor@flarehub.io>
2021-10-09 11:45:45 +0300

    Fix site link in admin header

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     // URL environment variable is needed because this is automatically provided by Netlify.
     // Take URL environment variable and make it available to the browser as SITE_URL.
     SITE_URL: process.env.URL,
-    BUILD_URL: process.env.DEPLOY_URL,
+    BUILD_URL: process.env.DEPLOY_PRIME_URL,
     // A path in the public folder that is not tracked in Git. Used to exclude e.g. large images
     LOCAL_PATH: process.env.LOCAL_PATH,
   },
